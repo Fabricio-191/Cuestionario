54862e6d55701f6fb4ad32ee82b8317b61e5cfa0
Fabricio-191 <fabricio19110@gmail.com>
2024-11-05 23:34:42 -0300

    -

--- out/CodeGen.cs
+++ out/CodeGen.cs
@@ -5,16 +5,31 @@ using System.IO;
 
 namespace Cuestionario {
 	public class CodeGenerator {
-		private string code;
+		private string code = "set /a score=0\nset /a last_answer=0\n";
 		
 		public void addCode(string code) {
 			Console.WriteLine(code);
-
 			this.code += code;
 		}
 
 		public void addPrint(string message) {
 			addCode("echo " + message + "\n");
+		}
+
+		public void addQuestion(string question) {
+			addPrint(question);
+		}
+
+		public void input() {
+			addCode("set /a last_answer=\n");
+		}
+
+		public void addCheckAnswer(string correctAnswer) {
+			addCode("if %last_answer% == " + correctAnswer + " (set /a score+=1)\n");
+		}
+
+		public void addFinalScore() {
+			addPrint("Your score is %score% out of %max_score%");
 		}
 
 		public void writeCode(string filename) {

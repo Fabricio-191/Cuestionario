3af5d51060268fbf57c5731b0384950ae14e82f0
Fabricio-191 <fabricio19110@gmail.com>
2024-11-19 16:11:13 -0300

    -

--- out/CodeGen.cs
+++ out/CodeGen.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Collections.Generic; 
 
 // generating .bat file
 
@@ -8,6 +9,11 @@ namespace Cuestionario {
 		private string code = "$score=0\n\n";
 
 		private int max_score = 0;
+		private string separator = "* ";
+
+		public void setSeparator(string separator) {
+			this.separator = separator;
+		}
 
 		private string expr = "";
 
@@ -38,29 +44,52 @@ namespace Cuestionario {
 			addCode("Write-Host \"" + message + "\"\n");
 		}
 
-		public void addOption(string option) {
-			addPrint("* " + option);
+		public void addPrint(string message, string color) {
+			addCode("Write-Host \"" + message + "\" -ForegroundColor " + color + " \n");
 		}
+
+		public void addOption(string option) {
+			addPrint(separator + option);
+		}
+
+		// -ForegroundColor Green
 
 		public void addFinalScore() {
 			addPrint("Your score is $score out of " + max_score);
 		}
 
-		public void addAnswerCheck(string correctAnswer, int value, bool includes) {
-			addCode("\n$last_answer = Read-Host \"Respuesta\"\n");
-			if(correctAnswer == ""){
+		private void checkIntegerInput(){
+			addCode("if ($last_answer -notmatch \"^\\d+$\"){\n");
+			addCode("  Throw \"Invalid input. Please enter a number\"\n");
+			addCode("}\n\n");
+		}
+
+		public void addQuestion(string correctAnswer, int value, bool includes, string name, Parser.Type inputType) {
+			// gen.addQuestion(question, correct, value, includes);
+			addCode("\n$last_answer = Read-Host \"\nRespuesta\"\n");
+			if(inputType == Parser.Type.integer){
+				checkIntegerInput();
+			}
+
+			if(name != null){
+				addCode("$" + name + " = $last_answer\n");
+			}
+
+			if(correctAnswer == null){
 				// no correct answer
 				addCode("\n\n\n");
 				return;
 			};
 
 			if(includes){
-				addCode("if ($last_answer.ToLower() -like \"*" + correctAnswer.ToLower() + "*\")");
+				addCode("if ($last_answer.ToLower() -like \"*" + correctAnswer + "*\")");
 			}else{
-				addCode("if ($last_answer.ToLower() -eq \"" + correctAnswer.ToLower() + "\")");
+				addCode("if ($last_answer.ToLower() -eq \"" + correctAnswer + "\")");
 			}
 			addCode("{ $score += " + value + " }\n\n\n\n");
 			max_score += value;
+
+			addPrint("\n\n");
 		}
 
 		public void writeCode(string filename) {
--- out/compilado/cuestionario.ps1
+++ out/compilado/cuestionario.ps1
@@ -1,58 +1,91 @@
 $score=0
 
-Write-Host "Cual es la capital de francia ?"
-Write-Host "* Paris"
-Write-Host "* Londres"
-Write-Host "* Madrid"
-Write-Host "* Berlin"
+Write-Host "Cual es la capital de francia ?" -ForegroundColor Yellow 
+Write-Host "-> Paris"
+Write-Host "-> Londres"
+Write-Host "-> Madrid"
+Write-Host "-> Berlin"
 
-$last_answer = Read-Host "Respuesta"
-if ($last_answer.ToLower() -eq "paris"){ $score += 1 }
-
-
-
-Write-Host "Cual es la capital de Alemania ?"
-Write-Host "* Paris"
-Write-Host "* Londres"
-Write-Host "* Madrid"
-Write-Host "* Berlin"
-
-$last_answer = Read-Host "Respuesta"
-if ($last_answer.ToLower() -eq "berlin"){ $score += 1 }
+$last_answer = Read-Host "
+Respuesta"
+if ($last_answer.ToLower() -eq "Paris"){ $score += 1 }
 
 
 
-Write-Host "Cuanto es 3 * 12 ?"
-Write-Host "* 36"
-Write-Host "* 24"
-Write-Host "* 48"
-Write-Host "* 12"
+Write-Host "
 
-$last_answer = Read-Host "Respuesta"
+"
+Write-Host "Cual es la capital de Alemania ?" -ForegroundColor Yellow 
+Write-Host "-> Paris"
+Write-Host "-> Londres"
+Write-Host "-> Madrid"
+Write-Host "-> Berlin"
+
+$last_answer = Read-Host "
+Respuesta"
+if ($last_answer.ToLower() -eq "Berlin"){ $score += 1 }
+
+
+
+Write-Host "
+
+"
+Write-Host "Cuanto es 3 * 12 ?" -ForegroundColor Yellow 
+Write-Host "-> 36"
+Write-Host "-> 24"
+Write-Host "-> 48"
+Write-Host "-> 12"
+
+$last_answer = Read-Host "
+Respuesta"
 if ($last_answer.ToLower() -eq "36"){ $score += 1 }
 
 
 
-Write-Host "Despeje la siguiente ecuacion: 2x + 3 = 7"
+Write-Host "
 
-$last_answer = Read-Host "Respuesta"
+"
+Write-Host "Despeje la siguiente ecuacion: 2x + 3 = 7" -ForegroundColor Yellow 
+
+$last_answer = Read-Host "
+Respuesta"
+if ($last_answer -notmatch "^\d+$"){
+  Throw "Invalid input. Please enter a number"
+}
+
 if ($last_answer.ToLower() -eq "2"){ $score += 2 }
 
 
 
-Write-Host "Cual es la estrella mas grande del sistema solar ?"
+Write-Host "
 
-$last_answer = Read-Host "Respuesta"
-if ($last_answer.ToLower() -like "*sol*"){ $score += 1 }
+"
+Write-Host "Cual es la estrella mas grande del sistema solar ?" -ForegroundColor Yellow 
 
-
-
-Write-Host "Cual es tu edad ?"
-
-$last_answer = Read-Host "Respuesta"
-
+$last_answer = Read-Host "
+Respuesta"
+$es = $last_answer
+if ($last_answer.ToLower() -like "*Sol*"){ $score += 1 }
 
 
-if ($res -lt 0){}else{}
+
+Write-Host "
+
+"
+Write-Host "Cual es tu edad ?" -ForegroundColor Yellow 
+
+$last_answer = Read-Host "
+Respuesta"
+if ($last_answer -notmatch "^\d+$"){
+  Throw "Invalid input. Please enter a number"
+}
+
+$res = $last_answer
+
+
+
+if ($res -lt 0){
+Write-Host "Edad invalida"}else{
+Write-Host "Tu edad en meses es: " ($res * 12)}
 
 Write-Host "Your score is $score out of 6"
